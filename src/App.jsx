c8a8162d17e5c5b5696c9626b84e70751f1ab8af
debabrata-pw08-429 <chintuthecoder@gmail.com>
2025-04-26 17:57:39 +0530

    feat: added logic  for removing edges

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import {
   ReactFlow,
   MiniMap,
@@ -7,6 +13,7 @@ import {
   useNodesState,
   useEdgesState,
   addEdge,
+  reconnectEdge,
 } from "@xyflow/react";
 import ProductList from "./components/ProductList/ProductList";
 import ProductNode from "./components/ProductNode/ProductNode";
@@ -14,6 +21,7 @@ import "@xyflow/react/dist/style.css";
 import "./App.css";
 
 export default function App() {
+  const edgeReconnectSuccessful = useRef(true);
   const [products, setProducts] = useState([]);
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
@@ -41,6 +49,23 @@ export default function App() {
 
   const nodeTypes = useMemo(() => ({ textUpdater: ProductNode }), []);
 
+  const onReconnectStart = useCallback(() => {
+    edgeReconnectSuccessful.current = false;
+  }, []);
+
+  const onReconnect = useCallback((oldEdge, newConnection) => {
+    edgeReconnectSuccessful.current = true;
+    setEdges((els) => reconnectEdge(oldEdge, newConnection, els));
+  }, []);
+
+  const onReconnectEnd = useCallback((_, edge) => {
+    if (!edgeReconnectSuccessful.current) {
+      setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+    }
+
+    edgeReconnectSuccessful.current = true;
+  }, []);
+
   return (
     <div className="app">
       <ProductList products={products} onAddNode={addNode} />
@@ -53,6 +78,9 @@ export default function App() {
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
           nodeTypes={nodeTypes}
+          onReconnect={onReconnect}
+          onReconnectStart={onReconnectStart}
+          onReconnectEnd={onReconnectEnd}
         >
           <Controls />
           <MiniMap />
